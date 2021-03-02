34ff0707d9af0cca8357c372a2536a174f061d6f
BPB Online <41231825+bpbpublications@users.noreply.github.com>
2021-03-02 11:15:32 +0530

    by BPB Publications

--- src/app/util.ts
+++
@@ -1,9 +0,0 @@
-import * as moment from 'moment';
-
-export function isToday(created) {
-    return moment(created).isSame(moment().startOf('day'), 'd');
-}
-
-export function isYesterday(created) {
-    return moment(created).isSame(moment().subtract(1, 'days').startOf('day'), 'd');
-}
